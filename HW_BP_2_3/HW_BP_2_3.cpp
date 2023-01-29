9e05c575e8e8b96415f287b574fbaec33a181558
Aleksandr Simarenko <alexandr.2001@mail.ru>
2023-01-29 16:12:35 +0300

    Initial commit

---
+++ HW_BP_2_3/HW_BP_2_3.cpp
@@ -0,0 +1,46 @@
+﻿// HW_BP_2_3.cpp : Задача 3. Вывод структуры
+
+#include <iostream>
+
+struct Address 
+{
+    std::string country;
+    std::string city;
+    std::string street;
+    int numbHouse;
+    int numbApart;
+    int postalCode;
+
+};
+
+void printAdddress(Address print) 
+{
+    std::cout << "Страна: " << print.country << std::endl;
+
+    std::cout << "Город: " << print.city << std::endl;
+
+    std::cout << "Улица: " << print.street << std::endl;
+
+    std::cout << "Номер дома: " << print.numbHouse << std::endl;
+
+    std::cout << "Номер квартиры: " << print.numbApart << std::endl;
+
+    std::cout << "Индекс: " << print.postalCode << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+
+    Address print1 = {"Россия", "Санкт-Петербург", "Заречная", 25, 77, 194363 };
+    
+    printAdddress(print1);
+
+    std::cout << std::endl;
+
+    Address print2 = { "Россия", "Москва", "Лениногорская", 5, 55, 111625 };
+
+    printAdddress(print2);
+
+    return 0;
+}
